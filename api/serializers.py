242dedb980e2fc9d51c7ffc6bc8e5457aa20c3ca
cns2382 <csanord@austin.utexas.edu>
2019-10-29 21:57:33 -0500

    Embed bookserializer inside event serializer

--- api/serializers.py
+++ api/serializers.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from api.models import Book, Event
 
 
-class EventSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Event
-        fields = '__all__'
-
-
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
+        fields = '__all__'
+
+
+class EventSerializer(serializers.ModelSerializer):
+    book = BookSerializer()
+
+    class Meta:
+        model = Event
         fields = '__all__'
