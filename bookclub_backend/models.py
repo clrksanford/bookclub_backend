11f5e06b964962df5085f56de06e3389f0747735
cns2382 <csanord@austin.utexas.edu>
2019-10-29 20:50:09 -0500

    Stub out initial models for book and event

---
+++ bookclub_backend/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    cover_url = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    blurb = models.CharField(max_length=500)
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    book = models.ForeignKey(to=Book)
+    date = models.DateField()
+    description = models.CharField(max_length=500)
