523f7a1c2f3c60afdf7085e52b958d40008d7dc8
Clark Sanford <clrksanford@gmail.com>
2019-11-11 21:31:01 -0600

    Create user model

--- api/models.py
+++ api/models.py
@@ -13,3 +13,10 @@ class Event(models.Model):
     book = models.ForeignKey(to=Book, null=True, on_delete=models.SET_NULL)
     date = models.DateField()
     description = models.CharField(max_length=500)
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=20)
+    books = models.ManyToManyField('Book', related_name='attendees')
