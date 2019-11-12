26bd68abb85c8d72b0d1086a889cf17db74e0330
Clark Sanford <clrksanford@gmail.com>
2019-11-11 22:34:59 -0600

    Create a post method for rsvping to events

--- api/views.py
+++ api/views.py
@@ -1,11 +1,24 @@
+from django.http import HttpResponse
+from django.views import View
+from rest_framework.decorators import action
 from rest_framework import viewsets
-from api.models import Book, Event
+from api.models import Book, Event, User
 from api.serializers import BookSerializer, EventSerializer
 
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all().order_by('-date')
     serializer_class = EventSerializer
+
+    @action(detail=True, methods=['post'])
+    def rsvp(self, request, pk=None):
+        event = self.get_object()
+        username = 'clrksanford'
+        user = User.objects.filter(username=username).first()
+        user.events.add(event)
+        user.save()
+
+        return HttpResponse(status=200)
 
 
 class BookViewSet(viewsets.ModelViewSet):
