08028f0d2129e35ddcf2baa6d07b8712b9635b5b
Clark Sanford <clrksanford@gmail.com>
2019-12-10 08:37:29 -0600

    Register all models with admin

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from api.models import User
+from api.models import Book, Event, User
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Book)
+admin.site.register(Event)
